6bd6d07e3ab637cef694a7b52bd3c108e8be38ff
paulspartan14 <e17081416@itmerida.edu.mx>
2021-05-21 13:37:56 -0500

    added documentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 const routerStrampi = require('./strampi')
+//Swagger documentation
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../swagger.json')
 
+// swagger
+router.use('/docs', swaggerUi.serve)
+router.get('/docs', swaggerUi.setup(swaggerDocument))
+
+// routes app
 router.get('/', (req, res) => {
   res.send({ message: 'Welcome Home!' })
 })
--- src/routes/strampi.js
+++ src/routes/strampi.js
@@ -2,11 +2,11 @@ const router = require('express').Router()
 
 const { getData, postData, postNewRegistry  } = require('../controllers/strampi')
 
-// endpoint para obtener los datos ya encriptados
-router.post('/validate', getData)
 // endpoint para obtener un hash unico en lacchain
 router.post('/encrypt', postData)
 // endpoint para encimar datos sobre los ya existentes
 router.post('/queue', postNewRegistry)
+// endpoint para obtener los datos ya encriptados
+router.post('/validate', getData)
 
 module.exports = router
