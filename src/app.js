ccc80e95f36b75a539dbacace681b2feba92c41e
paulspartan14 <e17081416@itmerida.edu.mx>
2021-05-21 16:02:28 -0500

    added cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const api = require('./routes')
 const { handleErrors } = require('./middlewares/handleErrors')
 
 // Middlewares
 app.use(express.json())
+
+// Cors options
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 // Routes
 app.use('/api/v1', api)
